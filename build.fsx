616b452fac56c752f1ef9ff479564ed888f5bfa3
spiffytech <spiffytech@gmail.com>
2014-09-24 21:27:13 -0400

    Create and delete a VM. Use a logger.

--- build.fsx
+++ build.fsx
@@ -14,6 +14,7 @@ Target "Clean" (fun _ ->
 // Default target
 Target "Default" (fun _ ->
     let references = [
+        "tools/NLog/lib/net45/NLog.dll";
     ]
 
     ["runner.fs"]
--- runner.fs
+++ runner.fs
@@ -4,6 +4,19 @@ namespace Runner
 module Helpers =
     let (@@) fn x = fn x
 
+module LoggerConfig =
+    open NLog
+    open NLog.Config
+    open NLog.Targets
+
+    let setLogLevel level =
+        let config = new LoggingConfiguration()
+        let consoleTarget = new ColoredConsoleTarget()
+        config.AddTarget("console", consoleTarget)
+        let rule = new LoggingRule("*", level, consoleTarget)
+        config.LoggingRules.Add rule
+        LogManager.Configuration <- config
+
 module CLI =
     type Status =
     | Success of string
@@ -20,6 +33,7 @@ module CLI =
         proc.StartInfo.Arguments <- "-c \"" + saniCmd + "\""
         proc.StartInfo.UseShellExecute <- false 
         proc.StartInfo.RedirectStandardOutput <- true
+        proc.StartInfo.RedirectStandardError <- true
         proc.Start() |> ignore
 
         // Capture output
@@ -34,9 +48,29 @@ module CLI =
         | 0 -> Success output
         | _ -> Failure (output, proc.ExitCode)
 
+module VirtualBox =
+    open NLog
+    let logger = LogManager.GetLogger("VirtualBox")
+
+    let createVM name =
+        CLI.runCmd @@ sprintf "VBoxManage createvm --name '%s' --register" name
+        |> printfn "%A"
+
+        logger.Info(sprintf "Created VM %s" name)
+
+    let deleteVM name =
+        CLI.runCmd @@ sprintf "VBoxManage unregistervm '%s' --delete" name
+        |> printfn "%A"
+
+        logger.Info(sprintf "Deleted VM %s" name)
+
 module main =
+    open NLog
+    LoggerConfig.setLogLevel LogLevel.Debug
+    let logger = LogManager.GetLogger("main")
+
     [<EntryPoint>]
     let main args =
-        CLI.runCmd """echo "Hello, World!" """
-        |> printfn "%A"
+        VirtualBox.createVM "runner"
+        VirtualBox.deleteVM "runner"
         0
