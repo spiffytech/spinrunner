3014cd89e1874fc43a6cfe82bdd1df8b65b88d0b
spiffytech <spiffytech@gmail.com>
2014-09-24 21:10:39 -0400

    Run arbitrary shell command and capture exit code

--- runner.fs
+++ runner.fs
@@ -1,11 +1,42 @@
-let proc = new System.Diagnostics.Process()
-proc.StartInfo.FileName <- "/bin/bash"
-proc.StartInfo.Arguments <- "-c \"" + "echo 'hello, world'" + "\""
-proc.StartInfo.UseShellExecute <- false 
-proc.StartInfo.RedirectStandardOutput <- true
-proc.Start()
-let sb = new System.Text.StringBuilder()
-while not proc.HasExited do
-    sb.Append(proc.StandardOutput.ReadToEnd())
-sb.Append(proc.StandardOutput.ReadToEnd())  // Do it again in case the process exits so quickly we never enter the loop
-printfn "%A" sb
+namespace Runner
+
+[<AutoOpen>]
+module Helpers =
+    let (@@) fn x = fn x
+
+module CLI =
+    type Status =
+    | Success of string
+    | Failure of (string * int)
+
+    let runCmd (cmd:string) =
+        let proc = new System.Diagnostics.Process()
+
+        // Escape quotes in the user command
+        let saniCmd = cmd.Replace("\"", "\\\"")
+
+        // Set up and run the command
+        proc.StartInfo.FileName <- "/bin/bash"
+        proc.StartInfo.Arguments <- "-c \"" + saniCmd + "\""
+        proc.StartInfo.UseShellExecute <- false 
+        proc.StartInfo.RedirectStandardOutput <- true
+        proc.Start() |> ignore
+
+        // Capture output
+        let sb = new System.Text.StringBuilder()
+        while not proc.HasExited do
+            sb.Append(proc.StandardOutput.ReadToEnd()) |> ignore
+        sb.Append(proc.StandardOutput.ReadToEnd()) |> ignore  // Do it again in case the process exits so quickly we never enter the loop
+
+        let output = sb.ToString().Trim()
+
+        match proc.ExitCode with
+        | 0 -> Success output
+        | _ -> Failure (output, proc.ExitCode)
+
+module main =
+    [<EntryPoint>]
+    let main args =
+        CLI.runCmd """echo "Hello, World!" """
+        |> printfn "%A"
+        0
